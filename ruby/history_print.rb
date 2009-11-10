c51a3beb2b9c63f7bd845ef351e355eed5a26f31
James Harrison <james@balvedastudios.com>
2009-11-10 11:00:07 +0000

    Consistency rocks

--- ruby/history_print.rb
+++ ruby/history_print.rb
@@ -1,9 +1,14 @@
 require 'rubygems'
 require 'amqp'
 require 'mq'
+# Handle signals for clean shutdown on ctrl+c
+Signal.trap('INT') { AMQP.stop{ EM.stop } }
+Signal.trap('TERM'){ AMQP.stop{ EM.stop } }
+# Start connection
 AMQP.start(:host => 'queue.eve-metrics.com') do
   puts "Connected to AMQP server OK, listening for new messages"
   amq = MQ.new
+  # Define queue and bind it to the fanout exchange for market history uploads
   amq.queue('CHANGEME_APP_NAME_GOES_HERE', :exclusive=>true, :auto_delete=>true).bind(amq.fanout('market_history_uploads')).subscribe do |msg|
     puts msg
   end
